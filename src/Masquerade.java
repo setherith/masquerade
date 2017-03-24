83005b12ac0c3f3f27321b0d5e61985f4dbb15d4
Shane Pudner <setherith@gmail.com>
2017-03-24 22:01:29 +0000

    Update and output example

--- src/Masquerade.java
+++ src/Masquerade.java
@@ -1,27 +1,33 @@
-/*Manifesto:
- * To take a username and convert it to a bitmap image for a profile
- * on any future sites for when a user doesn't add their own picture. */
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Masquerade {
 	public JPanel output;
 	public static String[] pixels;
+	private final int BLOCK_SIZE = 160;
+	private final Dimension SIZE = new Dimension(BLOCK_SIZE, BLOCK_SIZE);
 
 	public static void main(String[] args) {
+		new Masquerade();
+	}
+	
+	public Masquerade() {
 		
 		pixels = new String[16];
 		int pCount = 0;
 		
-		for (byte b : getCheckSum("abi")) {
+		for (byte b : getCheckSum("setherith")) {
 			if (b < 0)
 				b *= -1;
 			String fwd = String.format("%8s", Integer.toBinaryString(b)).replace(' ', '0');
@@ -33,16 +39,71 @@ public class Masquerade {
 			pCount++;
 		}
 		
-		new Masquerade();
+		JFrame app = new JFrame("Masquerade");
+		
+		output = new JPanel() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				DrawMask(g);
+			}
+		};
+		
+		output.setPreferredSize(SIZE);
+		
+		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		app.add(output);
+		app.pack();
+		
+		app.setLocationRelativeTo(null);
+		app.setVisible(true);
+		
+		WriteFile();
 	}
 
-	private static Color RandomColour() {
-		Random rand = new Random();
-		int r = rand.nextInt(255);
-		return new Color(r, r, r);
+	private void WriteFile() {
+		BufferedImage bi = new BufferedImage(SIZE.width, SIZE.height, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = bi.createGraphics();
+		
+		DrawMask(g);
+		
+		try
+		{
+			ImageIO.write(bi, "png", new File("test.png"));
+		} catch (Exception e) { 
+			e.printStackTrace();
+		}
 	}
 	
-	private static String Reverse(String forward) {
+	private Graphics DrawMask(Graphics g) {
+		g.setColor(RandomColour());
+		g.fillRect(0, 0, SIZE.width, SIZE.height);
+		
+		int y = 0;
+		
+		g.setColor(RandomColour());
+		for (String line : pixels) {
+			int x = 0;
+			for (char column : line.toCharArray()) {
+				if (column == '1') 
+					g.fillRect(x, y, 10, 10);
+				x += 10;
+				if (x > SIZE.width) x = 0;
+			}
+			y += 10;
+			if (y > SIZE.height) y = 0;
+		}
+		return g;
+	}
+	
+	private Color RandomColour() {
+		Random rand = new Random();
+		return new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
+	}
+	
+	private String Reverse(String forward) {
 		String result = "";
 		char[] chars = forward.toCharArray();
 		for (int i = chars.length - 1; i >= 0; i--) {
@@ -51,51 +112,13 @@ public class Masquerade {
 		return result;
 	}
 
-	public Masquerade() {
-		JFrame app = new JFrame("NameGraphic");
-		
-		output = new JPanel() {
-			@Override
-			public void paintComponent(Graphics g) {
-				super.paintComponent(g);
-				
-				g.setColor(RandomColour());
-				g.fillRect(0, 0, 160, 160);
-				
-				int y = 0;
-				
-				g.setColor(RandomColour());
-				for (String line : pixels) {
-					int x = 0;
-					for (char column : line.toCharArray()) {
-						if (column == '1') 
-							g.fillRect(x, y, 10, 10);
-						x += 10;
-						if (x > 160) x = 0;
-					}
-					y += 10;
-					if (y > 160) y = 0;
-				}
-				
-			}
-		};
-		
-		output.setPreferredSize(new Dimension(160, 160));
-		
-		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		app.add(output);
-		app.pack();
-		
-		app.setLocationRelativeTo(null);
-		app.setVisible(true);
-	}
+	
 
-	public static byte[] getCheckSum(String name) {
+	private byte[] getCheckSum(String name) {
 		try {
 			MessageDigest md = MessageDigest.getInstance("MD5");
 			return md.digest(name.getBytes());
-		} catch (NoSuchAlgorithmException e) {
-		}
+		} catch (NoSuchAlgorithmException e) { }
 		return null;
 	}
 }
