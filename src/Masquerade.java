ee6184acae3e53ec89f678286f3fb3b65537c2b3
Shane Pudner <setherith@gmail.com>
2017-03-25 08:25:58 +0000

    File name incl. hex codes

--- src/Masquerade.java
+++ src/Masquerade.java
@@ -1,7 +1,6 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.security.MessageDigest;
@@ -14,20 +13,24 @@ import javax.swing.JPanel;
 
 public class Masquerade {
 	public JPanel output;
-	public static String[] pixels;
+	public String[] pixels;
 	private final int BLOCK_SIZE = 160;
 	private final Dimension SIZE = new Dimension(BLOCK_SIZE, BLOCK_SIZE);
+	private static String username;
+	private Color background;
+	private Color foreground;
 
 	public static void main(String[] args) {
-		new Masquerade();
+		username = "serenity";
+		new Masquerade(username);
 	}
 	
-	public Masquerade() {
+	public Masquerade(String name) {
 		
 		pixels = new String[16];
 		int pCount = 0;
 		
-		for (byte b : getCheckSum("setherith")) {
+		for (byte b : getCheckSum(name)) {
 			if (b < 0)
 				b *= -1;
 			String fwd = String.format("%8s", Integer.toBinaryString(b)).replace(' ', '0');
@@ -71,19 +74,29 @@ public class Masquerade {
 		
 		try
 		{
-			ImageIO.write(bi, "png", new File("test.png"));
+			String fc = ConvertColourToString(foreground);
+			String bc = ConvertColourToString(background);
+			String filename = username + bc + fc + ".png";
+			ImageIO.write(bi, "png", new File(filename));
 		} catch (Exception e) { 
 			e.printStackTrace();
 		}
+		
+	}
+	
+	private String ConvertColourToString(Color input) {
+		return '#' + Integer.toHexString(input.getRGB()).substring(2);
 	}
 	
 	private Graphics DrawMask(Graphics g) {
-		g.setColor(RandomColour());
+		background = RandomColour();
+		g.setColor(background);
 		g.fillRect(0, 0, SIZE.width, SIZE.height);
 		
 		int y = 0;
 		
-		g.setColor(RandomColour());
+		foreground = RandomColour();
+		g.setColor(foreground);
 		for (String line : pixels) {
 			int x = 0;
 			for (char column : line.toCharArray()) {
@@ -111,8 +124,6 @@ public class Masquerade {
 		}
 		return result;
 	}
-
-	
 
 	private byte[] getCheckSum(String name) {
 		try {
